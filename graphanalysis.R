7730b979f95c1c9aacd1b1e78f8231db8be7f9ae
Michael <rozzu97@live.it>
2021-10-26 10:33:29 +0200

    cleandata

--- graphanalysis.R
+++ graphanalysis.R
@@ -2,26 +2,27 @@
 setwd("~/Documents/GitHub/NPS-project")
 
 load("~/Documents/GitHub/NPS-project/cleandata.Rdata")
+cleandata=import("cleandata.Rdata")
 View(cleandata)
 
 
 ##1)prima cosa faccio uno scatterplot
 #num volontari vs qtà plastica raccolta
 quartz()
-plot(cleandata$TotalVolunteers,cleandata$Totalltems_EventRecord)
+plot(cleandata$TotalVolunteers,cleandata$TotalItems)
 
 sort(cleandata$TotalVolunteers, decreasing = TRUE)
-sort(cleandata$Totalltems_EventRecord, decreasing = TRUE)
+sort(cleandata$TotalItems, decreasing = TRUE)
 
 noout=cleandata[which(cleandata$TotalVolunteers < 5000),]
-noout=noout[which(noout$Totalltems_EventRecord < 100000),]
+noout=noout[which(noout$TotalItems < 100000),]
 
 #anomalia: alcune spedizioni hanno zero item raccolti, ovviamente le elimino
 #idem per zero persone e item diversi da zero
-noout=noout[which(noout$Totalltems_EventRecord != 0),]
+noout=noout[which(noout$TotalItems != 0),]
 noout=noout[which(noout$TotalVolunteers != 0),]
 
-plot(noout$TotalVolunteers,(noout$Totalltems_EventRecord), main="Human resouches VS Items collected",
+plot(noout$TotalVolunteers,(noout$TotalItems), main="Human resouches VS Items collected",
      xlab="Num of total volunteers", ylab="Items recorded")
 #metto un logaritmo solo nella quantità di plastica raccolta
 #non lo metto anche per i volontari perchè mi sembra concettualmente sbagliato (num volontari è intero)
@@ -29,8 +30,8 @@ plot(noout$TotalVolunteers,(noout$Totalltems_EventRecord), main="Human resouches
 
 ##altrimenti
 zoom=noout[which(noout$TotalVolunteers < 500),]
-zoom=zoom[which(zoom$Totalltems_EventRecord < 10000),]
-plot(zoom$TotalVolunteers,(zoom$Totalltems_EventRecord), main="Human resouches VS Items collected",
+zoom=zoom[which(zoom$TotalItems < 10000),]
+plot(zoom$TotalVolunteers,(zoom$TotalItems), main="Human resouches VS Items collected",
      xlab="Num of total volunteers", ylab="Items recorded")
 #meglio ancora!
 
