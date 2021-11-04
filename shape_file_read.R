a44fc3a7cebd60f95186cbff5b71e17b799ff3aa
CrazyChela97 <michela.frigeri@mail.polimi.it>
2021-11-04 13:53:04 +0100

    prova solo nord america

--- shape_file_read.R
+++ shape_file_read.R
@@ -8,13 +8,15 @@ library(sp)
 library(shp2graph)
 library(lubridate)
 
-setwd("~/Documents/Politecnico/Magistrale/Non Parametric Statistics/PROJECT")
+setwd("~/Documents/GitHub/NPS-project/TM_WORLD_BORDERS_SIMPL-0")
 shape_file <- readOGR("EC2020_All_Cleanup_Events_2015_2018.shp", GDAL1_integer64_policy = TRUE)
 
 
 shape2 =readOGR("TM_WORLD_BORDERS_SIMPL-0.3.shp")
+summary(shape2)
 plot(shape2)
+Nord_America = shape2[which(shape2@data$NAME=='Georgia'), ]
 points(shape_file, pch=20)
-
+plot(Nord_America)
 
 ##sf pacchetto per shapefiles
