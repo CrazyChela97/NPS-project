f6e3285d4ee2ef87d0f29a6dd10a92eb00f03d1b
CrazyChela97 <michela.frigeri@mail.polimi.it>
2022-01-22 17:48:31 +0100

    Conformal is done + robust regression on plastic

--- Conformal Prediction.R
+++ Conformal Prediction.R
@@ -89,41 +89,6 @@ matlines(volunt.grid ,cbind(c_preds_split$up,c_preds_split$lo) ,lwd =1, col =" r
 
 
 
-# Poly --------------------------------------------------------------------
-##model 2 - 3: Poly and Natural splines con regressori 
-test_data = data[which(data$Year == 2018), ]
-train_data = data[which(data$Year == 2016 | data$Year == 2017), ]
-
-x <- train_data$TotalVolunteers
-y <- train_data$log_item
-
-model_poly <- lm(y ~ poly(x , degree=8)+ EventType + weekend + Season, data=train_data)
-
-train_poly=function(x1,y1,out=NULL){
-  lm(y1 ~ poly(x1 , degree=8) + train_data$EventType + train_data$weekend + train_data$Season)
-}
-
-predict_poly=function(obj, new_x){
-  predict(obj,new_x)$y
-}
-
-#pippo=train_ss(x,y)
-#predict_ss(pippo, as.data.frame(train_data$TotalVolunteers))
-
-#questo runna in 5 ore e un quarto raga
-volunt.grid = seq(range(x)[1],range(y)[2], length.out = dim(train_data)[1])
-
-design_matrix = matrix(poly(x , degree=8), nrow=length(y))
-pred_grid = matrix(poly(x,degree=8,coefs = attr(poly(x,degree=8),"coefs") ),ncol=2)
-
-c_preds=conformal.pred(design_matrix, y, pred_grid, alpha=0.05,
-                       verbose=T,train.fun = train_poly, predict.fun = predict_poly, num.grid.pts = 200)
-
-plot(x, y, xlim=range(volunt.grid), cex = 0.5, col="darkgrey ")
-lines(volunt.grid, c_preds$pred, lwd =2, col ="red", lty=3)
-matlines(volunt.grid, cbind(c_preds$up,c_preds$lo) ,lwd =1, col =" blue",lty =3)
-
-
 
 # Poly Model - COMPLETE -------------------------------------------------------------
 
@@ -133,25 +98,35 @@ lm_predict = lm.funs(intercept = T)$predict.fun
 # Design Matrix with Regressors
 dummies = dummy_cols(train_data, select_columns = c('EventType', 'Season'))
 design_matrix = matrix(poly(x.train, degree=8), ncol=8)
-design_matrix = cbind(design_matrix, dummies[ ,c(10, 12:14, 16:18)], train_data$weekend)
+design_matrix = cbind(design_matrix, dummies[ ,c(10, 12:14, 16:18)])
 design_matrix = as.matrix(design_matrix)
 # Design Matrix of New Obs
 dummies = dummy_cols(test_data, select_columns = c('EventType', 'Season'))
 pred_grid = matrix(poly(x.test, degree=8, coefs = attr(poly(x.train, degree=8), "coefs")), ncol=8)
-pred_grid = cbind(pred_grid, dummies[ ,c(10, 12:14, 16:18)], test_data$weekend)
+pred_grid = cbind(pred_grid, dummies[ ,c(10, 12:14, 16:18)])
 pred_grid = as.matrix(pred_grid)
 
-c_preds = conformal.pred(design_matrix, y.train, pred_grid, alpha=0.05, verbose=T, 
+c_preds_poly = conformal.pred(design_matrix, y.train, pred_grid, alpha=0.05, verbose=T, 
                          train.fun=lm_train, predict.fun=lm_predict, num.grid.pts = 200)
 
-plot(x.test, y.test, ylim=range(c(c_preds$up, c_preds$lo)), cex = 0.5, col="darkgrey")
-plotCI(x.test, c_preds$pred, ui=c_preds$up, li=c_preds$lo, add=TRUE, pch=16)
+plot(x.test, y.test, ylim=range(c(c_preds_poly$up, c_preds_poly$lo)), cex = 0.5, col="darkgrey")
+plotCI(x.test, c_preds_poly$pred, ui=c_preds_poly$up, li=c_preds_poly$lo, add=TRUE, pch=16)
+
+
+# GOF check
+excluded = 0
+for (i in 1:length(y.test)){
+  excluded = excluded + (y.test[i]<c_preds_poly$lo[i]) + (y.test[i]>c_preds_poly$up[i])
+}
+exc.perc.ns = excluded/length(y.test)
+exc.perc.ns # 5.6 %
+
 
 
 
 # Natural Splines - NO Covariates -----------------------------------------
-knots = quantile(x, probs=c(seq(0.35, 0.95, by=0.1), 0.98, 0.99, 0.996))
-boundary_knots = quantile(x, probs=c(0.01, 0.999))
+knots = quantile(x.train, probs=c(seq(0.35, 0.95, by=0.1), 0.98, 0.99, 0.996))
+boundary_knots = quantile(x.train, probs=c(0.01, 0.999))
 knots
 boundary_knots
 
@@ -195,3 +170,41 @@ matlines(x.grid, cbind(c_preds$up,c_preds$lo), lwd=2, col="blue", lty =3)
 
 
 # Natural Splines - COMPLETE -----------------------------------------
+
+lm_train = lm.funs(intercept = T)$train.fun
+lm_predict = lm.funs(intercept = T)$predict.fun
+
+# Design Matrix with Regressors
+dummies = dummy_cols(train_data, select_columns = c('EventType', 'Season'))
+design_matrix = ns(x.train, knots=knots, Boundary.knots=boundary_knots)
+design_matrix = cbind(design_matrix, dummies[ ,c(10, 12:14, 16:18)])
+design_matrix = as.matrix(design_matrix)
+# Design Matrix of New Obs
+dummies = dummy_cols(test_data, select_columns = c('EventType', 'Season'))
+pred_grid = matrix(ns(x.test, knots=knots, Boundary.knots=boundary_knots), nrow=length(x.test))
+pred_grid = cbind(pred_grid, dummies[ ,c(10, 12:14, 16:18)])
+pred_grid = as.matrix(pred_grid)
+
+c_preds_ns = conformal.pred(design_matrix, y.train, pred_grid, alpha=0.05, verbose=T, 
+                         train.fun=lm_train, predict.fun=lm_predict, num.grid.pts = 200)
+
+c_preds_ns = c_preds
+
+plot(x.test, y.test, ylim=range(c(c_preds_ns$up, c_preds_ns$lo)), 
+     pch=16, cex = 0.7, col="deeppink")
+plotCI(x.test, c_preds_ns$pred, ui=c_preds_ns$up, li=c_preds_ns$lo, add=TRUE, 
+       pch=16, cex=0.7, slty=2, scol='darkgrey', col='black')
+
+
+# GOF check
+excluded = 0
+for (i in 1:length(y.test)){
+  excluded = excluded + (y.test[i]<c_preds_ns$lo[i]) + (y.test[i]>c_preds_ns$up[i])
+}
+exc.perc.ns = excluded/length(y.test)
+exc.perc.ns # 5.6 %
+
+
+
+y_fit_test = c_preds$pred
+
--- Plastic Model.R
+++ Plastic Model.R
@@ -2,6 +2,23 @@
 ###       PLASTIC  PREDICTION  MODEL          ###
 #################################################
 
+
+
+# Packages ----------------------------------------------------------------
+
+library(robustbase)
+library(psych)
+library(MASS)
+library(ellipse)
+library(here)
+library(DescTools)
+library(knitr)
+library(RobStatTM)
+
+
+
+# Data Analysis -----------------------------------------------------------
+
 CleanUsa = import("CleanUsa.Rdata")
 
 plastic.perc = CleanUsa$`%Plastic&Foam`/100
@@ -11,15 +28,18 @@ plastic.items = round(CleanUsa$TotalClassifiedItems_EC2020 * plastic.perc)
 plot(CleanUsa$TotalVolunteers, plastic.perc) # no sense
 plot(log(CleanUsa$TotalItems), plastic.perc) # no sense
 
+
 # considerando numero items plastica
 plot(CleanUsa$TotalVolunteers, log(plastic.items))
 # plastic vs volunteers : same shape as total.items vs volunteers
-plot(log(CleanUsa$TotalItems), log(plastic.items))
+plot(log(CleanUsa$TotalItems[CleanUsa$Year==2018]), log(plastic.items[CleanUsa$Year==2018]))
 # plastic vs total.items : nearly linear relation as expected
-
+plot(y_fit_test, log(plastic.items[CleanUsa$Year==2018]))
+# più un casino ma forse più corretto ? 
 
 data = import("RegData.Rdata")
-data$plastic_items = plastic.items
+plastic.items[plastic.items==0] = 1
+data$log_plastic = log(plastic.items)
 
 # Analysis for Categorical Regressors -------------------------------------
 
@@ -90,3 +110,49 @@ points(Marine$log_item, log(Marine$plastic_items), col = 'orange1')
 points(Water$log_item, log(Water$plastic_items), col = 'blue')
 points(Under$log_item, log(Under$plastic_items), col = 'red3')
 
+
+
+
+# Outliers Robust ---------------------------------------------------------
+
+# Minimum Covariance Determinant (MCD)
+dati = data.frame(cbind(x.test, y.test))
+fit_MCD = covMcd(x = dati, alpha = .75, nsamp = "best")
+fit_MCD
+# plot dati
+ind_best_subset <- fit_MCD$best
+N <- nrow(dati)
+p <- ncol(dati)
+plot(dati, col = ifelse(1:N %in% ind_best_subset, "black", "red"), pch=19, cex=0.5)
+
+# MOLTO BAD : non lo userei
+
+# Robust Regression -------------------------------------------------------
+
+# We use data from 2016 & 2017 as training dataset for the models
+# We then test the GOF of the model using 2018 data
+test_data = data[which(data$Year == 2018), ]
+train_data = data[which(data$Year == 2016 | data$Year == 2017), ]
+
+y.train = train_data$log_plastic
+x.train = train_data$log_item
+y.test = test_data$log_plastic
+x.test = test_data$log_item
+x.grid = seq(range(x.test)[1], range(x.test)[2], by=1)
+
+# basic OLS
+fit_lm = lm(log_plastic ~ log_item, data=test_data)
+# LMS : least median of squares
+fit_lms = lmsreg(log_plastic ~ log_item, data=test_data)
+# LTS : least trimmed squares
+fit_lts = ltsReg(log_plastic ~ log_item, alpha=.75, mcd=TRUE, data=test_data)
+
+# plot comparison
+plot(x.test, y.test, col='darkgrey')
+abline(fit_lm, col="red", lwd=2)
+abline(fit_lms, col="darkblue", lwd=2)
+abline(fit_lts, col="darkgreen", lwd=2)
+legend("bottomright", c('OLS', 'LMS', 'LTS'), lwd=rep(2,4), col=c("red", "darkblue", "darkgreen"))
+
+# graphical analysis
+plot(fit_lts)
